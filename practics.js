07debee592b03d660d8e05320309eb32db73ab63
Imran <mohdimranshountoo@gmail.com>
2024-02-08 12:51:36 +0530

    updated code

--- practics.js
+++ practics.js
@@ -11,15 +11,124 @@ text += "</ul>";
 document.getElementById("demo").innerHTML = text;
 
 fruits.shift();
-document.getElementById("demo2").innerHTML = fruits;
 
-fruits.unshift("lemon");
+text = "<ul>";
+fruits.forEach(myFunction);
+
+text += "</ul>";
+
+
+document.getElementById("demo2").innerHTML = text;
+
+
+
+
+fruits.unshift("lemon")
+
 fruits[0] = "guava"
 fruits[8] = "tomato"
-document.getElementById("demo3").innerHTML = fruits
+
+text = "<ul>"
+fruits.forEach(myFunction)
+text += "</ul>"
+document.getElementById("demo3").innerHTML = text
 
 const myGirls = ["Cecilie", "Lone", "Linus"];
 const myBoys = ["Emil", "Tobias", "Linus"];
 const myChildren = myGirls.concat(myBoys);
 
 document.getElementById("demo4").innerHTML = myChildren;
+console.log(document)
+
+const demo4 = document.getElementById("demo4")
+
+document.title = "mypage"
+const myArray = [1,3,5,7,90,67,45]
+const sqArray = myArray.map(myFunction2)
+
+function myFunction2(value){
+    return value * 2
+}
+
+document.getElementById("demo5").innerHTML = sqArray
+
+function filterFunc(value){
+    return value > 50
+
+}
+
+const over50 = myArray.filter(filterFunc)
+
+document.getElementById("demo6").innerHTML = over50
+
+
+switch (new Date().getDay()) {
+    case 6:
+      text = "Today is Saturday";
+      break;
+    case 0:
+      text = "Today is Sunday";
+      break;
+    default:
+      text = "Looking forward to the Weekend";
+  }
+
+  document.getElementById("demo7").innerHTML = text
+
+  switch (new Date().getDay()) {
+    case 4:
+    case 5:
+      text = "Soon it is Weekend";
+      break;
+    case 0:
+    case 6:
+      text = "It is Weekend";
+      break;
+    default:
+      text = "Looking forward to the Weekend";
+  }
+
+  
+  document.getElementById("demo8").innerHTML = text
+
+
+
+  // Create a Map
+const fruitsNew = new Map([
+  ["apples", 500],
+  ["bananas", 300],
+  ["oranges", 200]
+]);
+
+document.getElementById("body").innerHTML = fruitsNew.get("apples");
+
+try {
+  alert("Welcome guest!");
+}
+catch(err) {
+  document.getElementById("body").innerHTML = err.message;
+}
+
+try {
+  aslert("Welcome guest!");
+}
+catch(err) {
+  document.getElementById("body").innerHTML = err;
+}
+
+
+
+let num = 1;
+try {
+  num.toUpperCase();
+}
+catch(err) {
+  document.getElementById("demo9").innerHTML = err.name;
+}
+
+try {
+  eval("alert('Hello)");   // Missing ' will produce an error
+}
+catch(err) {
+  document.getElementById("demow").innerHTML = err.name;
+}
